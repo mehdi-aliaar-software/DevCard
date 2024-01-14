0572f503d6eac6a7264f32bcb26fd8ebed5de9b2
mehdi.aliaar <mehdi.aliaar@outlook.com>
2024-01-14 19:35:41 +0330

    projects and latestArticles moved to view component

--- DevCArd_MVC/Models/Article.cs
+++ DevCArd_MVC/Models/Article.cs
@@ -1,9 +1,22 @@
-﻿namespace DevCard_MVC.Models
+﻿using System.Xml.Linq;
+
+namespace DevCard_MVC.Models
 {
     public class Article
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
+
+
+        public Article(int id, string title, string description, string image)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+            Image = image;
+        }
     }
 }
---
+++ DevCArd_MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article(1,"Teaching AspNet", "this is one of the best courses you must learn.","blog-post-thumb-card-1.jpg" ),
+                new Article(2,"Teaching Git", "this is the urgent courses you must learn.","blog-post-thumb-card-2.jpg" ),
+                new Article(3,"Teaching AI", "what you need to acquire to go top.","blog-post-thumb-card-3.jpg" ),
+                new Article(4,"Teaching Data Analysis", "this is one of the best courses you must learn.","blog-post-thumb-card-4.jpg" )
+            };
+
+
+            return View("_LatestArticles", articles);
+        }
+    }
+}

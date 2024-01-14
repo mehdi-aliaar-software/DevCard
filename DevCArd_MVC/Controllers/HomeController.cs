caf57ca81bec8627e64d850e00df4af2e5a2a4ac
mehdi.aliaar <mehdi.aliaar@outlook.com>
2024-01-14 23:06:09 +0330

    contact validation done

--- DevCArd_MVC/Controllers/HomeController.cs
+++ DevCArd_MVC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Mime;
 
 namespace DevCard_MVC.Controllers
 {
@@ -18,9 +19,54 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Contact()
         {
-            return View();
+            var model = new Contact();
+            return View(model);
+        }
+
+        //[HttpPost]
+        //public JsonResult Contact(IFormCollection form)
+        //{
+        //    return Json(Ok());
+        //}
+
+        [HttpPost]
+        public JsonResult Contact(Contact contact)
+        {
+            Console.WriteLine(contact.ToString());
+            return Json(Ok());
+        }
+
+        public FileResult SampleFile()
+        {
+            //return File("file-place");
+            var fileByte = System.IO.File.ReadAllBytes("wwwroot/test.txt");
+
+            const string fileName = "testFile.txt";
+            return File(fileByte, MediaTypeNames.Text.Plain, fileName);
+        }
+
+        public JsonResult SampleJson()
+        {
+            return Json(new { 
+                Id=12, 
+                name="matt liaarson", 
+                grade="Master", 
+                role="developer" ,
+                website="abc.com"});
+        }
+
+
+        public RedirectResult SampleRedirect()
+        {
+            return Redirect("https://www.google.com");
+        }
+
+        public RedirectToActionResult SampleRedirectToAction()
+        {
+            return RedirectToAction("Contact", "Home");
         }
 
 
---
+++ DevCArd_MVC/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevCard_MVC.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "this data must be applied")]
+        [MinLength(3, ErrorMessage= "at least 3 characters")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "a correct email format must be applied")]
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public string Service { get; set; }
+    }
+}

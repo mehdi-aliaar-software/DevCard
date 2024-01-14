10b87f1e247abdca9db32661f4d5b37124d52f52
mehdi-aliaar-software <mehdi.aliaar.software@gmail.com>
2024-01-14 16:55:41 +0330

    using view component for projects

---
+++ DevCArd_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+	public class ProjectsViewComponent:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var projects = new List<Project>
+			{
+				new Project(1,"Taxi Auto Drive", "درخواست تاکسی خودران برای سفرهای درون شهری","project-1.jpg" , "LiaarsonX"),
+				new Project(2,"Zood Food", "درخواست غذای فوری برای محدوده شهر مشهد","project-2.jpg" , "Restaurants"),
+				new Project(3,"Schools", "سیستم هوشمند  اداره مدارس کشور","project-3.jpg" , "Secretary"),
+				new Project(4,"Space OX", "پروژه طراحی سفینه های فضایی ","project-4.jpg" , "NASA")
+			};
+
+			return View("_Projects", projects);
+		}
+	}
+}
